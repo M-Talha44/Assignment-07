51882c4fc696bfb757136bb6e833a98f48fe693c
M-Talha44 <shktalhaweb@gmail.com>
2025-01-16 22:54:13 +0500

    coin

--- Programs/8. Coin Toss Simulator/script.js
+++ Programs/8. Coin Toss Simulator/script.js
@@ -1,2 +1,13 @@
 var a=prompt(" Input \n 1. For 'Heads'  \n 0. For 'Tails'");
-let b=Math.floor(Math.random()*2);
+var b=Math.floor(Math.random()*2);
+if (b==1) {
+    console.log("Heads");
+} else {
+    console.log("Tails");
+    
+}
+
+
+    
+        
+
